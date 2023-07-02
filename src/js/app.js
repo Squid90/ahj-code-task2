ae988b2831b1409be0130120135478bcea2b25ea
Squid90 <squid90@rambler.ru>
2023-07-02 19:26:46 +0300

    new

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,30 @@
 // TODO: write code here
 
 (() => {
-    let playing = true,
-      activeHole = 1;
-  
-    const stop = () => playing = true,
-      deactivateHole = () => document.querySelector('.hole_has-goblin').classList.remove('hole_has-goblin'),
-      activateHole = index => document.querySelector(`.hole${index}`).classList.add('hole_has-goblin'),
-      next = () => setTimeout(() => {
-        if ( !playing ) {
-          return;
-        }
-        deactivateHole();
-        activeHole = Math.floor( 1 + Math.random() * 16 );
-        activateHole( activeHole );
-        next();
-      }, 800 );
-  
-    next();
-  })();
+  const playing = true;
+  let activeHole = 1;
+
+  if (playing) {
+    const deactivateHole = () => document.querySelector('.hole_has-goblin').classList.remove('hole_has-goblin');
+    const activateHole = (index) => document.querySelector(`.hole${index}`).classList.add('hole_has-goblin');
+    setInterval(() => {
+      if (!playing) {
+        return;
+      }
+      deactivateHole();
+      activeHole = Math.floor(1 + Math.random() * 16);
+      activateHole(activeHole);
+    }, 800);
+  }
+})();
 
 for (let index = 1; index < 17; index += 1) {
-    const hole = document.querySelector(`.hole${index}`);
-    hole.onclick = function() {
-        if(hole.classList.contains('hole_has-goblin')) {
-            alert ("You've catched it");
-        } else {
-          alert ("You've missed it");
-        };
-      };
+  const hole = document.querySelector(`.hole${index}`);
+  hole.onclick = function () {
+    if (hole.classList.contains('hole_has-goblin')) {
+      alert("You've catched it");
+    } else {
+      alert("You've missed it");
+    }
   };
+}
